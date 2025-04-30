3f937eceec2e5472aefb10115b2ab1123d39e3f3
Ahmedsamehm <a7medxd992@hotmail.com>
2025-04-30 06:38:14 +0300

    Updates

--- assets/Header-QfPSsCEn.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BxtrJGGV.js";function l({children:t,className:x}){return e.jsx("h1",{className:`${x} text-xl sm:text-2xl md:text-xl lg:text-2xl font-bold`,children:t})}export{l as H};
